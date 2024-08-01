c4d182d5c98721901852399e68a2e15cb691befa
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-08-01 11:49:55 +0300

    added redirection from main activity

--- app/src/main/java/com/example/mlseriescamera/MainActivity.kt
+++ app/src/main/java/com/example/mlseriescamera/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.mlseriescamera
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mlseriescamera.databinding.ActivityMainBinding
+import com.example.mlseriescamera.helpers.ImageHelperActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        //setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnGoToImageActivity.setOnClickListener {
+            onClickGoToImageActivity()
+        }
+    }
+
+    private fun onClickGoToImageActivity() {
+        Intent(this, ImageHelperActivity::class.java).also {
+            startActivity(it)
         }
     }
 }
